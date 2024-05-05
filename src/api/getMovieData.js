b373d3aceb4b7ef7a47313f32bfa8789f62fd60a
FEMOR8 <felixmorau@gmail.com>
2024-05-05 10:26:09 +0200

    fetch api from https

--- src/api/getMovieData.js
+++ src/api/getMovieData.js
@@ -1,5 +1,5 @@
 const getMovieData = (movieTitle) => {
-  const url = `http://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=${process.env.NEXT_PUBLIC_OMDB_KEY}`;
+  const url = `https://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=${process.env.NEXT_PUBLIC_OMDB_KEY}`;
 
   const movieData = fetch(url)
     .then((response) => response.json())
